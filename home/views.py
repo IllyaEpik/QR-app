93092b6504dba79990f320e365799aea596dbb74
markpopovich9 <betmen070809@gmail.com>
2025-01-23 21:58:03 +0300

    Create auth and logout

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def view_home(request):
-    return render(request,template_name= "home/index.html")
+    name = request.user
+    return render(request,template_name= "home/index.html", context={"name": name})
--- user/views.py
+++ user/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login
+from django.db.utils import IntegrityError
 # Create your views here.
 
 def authorization(request):
-
-    return render(request,template_name= "user/authorization.html")
+        error = ""
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username= username, password = password)
+            if user:
+                login(request, user)
+                return redirect('welcome')
+            else:
+                error = "Username or password is not correct"
+        
+        return render(request,template_name= "user/authorization.html", context={"error": error})
     
 def registration(request):
+    add = ""
     if request.method == "POST":
-        username= request.POST.get("username")
-        password= request.POST.get("password")
-        User.objects.create_user( username=username, password=password)
-    return render(request,template_name= "user/registration.html")
+        try:
+    
+            username= request.POST.get("username")
+            password= request.POST.get("password")
+            email = request.POST.get("email")
+            print(username,password)
+            User.objects.create_user( username=username, password=password,email=email)
+        except IntegrityError:
+
+            add = "Username already excists"
+    return render(request,template_name= "user/registration.html", context={"add": add})
+
+def logout(request):
+    if request.user:
+        logout(request)
+        return redirect('login')
 
