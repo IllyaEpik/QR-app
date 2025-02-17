ba29c58265742231c7b6d22aa046246e9abcf565
markpopovich9 <betmen070809@gmail.com>
2025-02-17 17:12:04 +0300

    Filter

---
+++ QR_cods/static/QR-cods/my.js
@@ -0,0 +1,18 @@
+let qrCards = document.querySelectorAll('.qr-card')
+let inp = document.querySelector('input')
+let fil = document.querySelector('.filter')
+
+
+fil.addEventListener("click", (e) =>{
+   for (let qrCard of qrCards ){
+    console.log(qrCard.id.split(inp.value))
+   if (qrCard.id.split(inp.value).length>1){
+    qrCard.classList.remove("hidden")
+    }else{
+        qrCard.classList.add("hidden")
+    }
+} 
+})
+
+
+
