efba55bfc94b197a97566b95edd2cb0045908f81
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-03-02 15:16:45 +0200

    all is good

---
+++ subscriptions/static/subscriptions/script.js
@@ -0,0 +1,31 @@
+let buttons = document.querySelectorAll("button")
+let form = document.querySelector('form')
+let bg = document.querySelector('.bg')
+let hide = document.querySelector("#hide")
+let count = document.querySelector('.count')
+console.log(hide)
+for (let button of buttons){
+    console.log(button.name)
+    if (button.name === "subscription"){
+        button.addEventListener('click',(event) => {
+            form.classList.remove('hidden')
+            bg.classList.remove('hidden')
+            hide.value = event.target.value
+            if (hide.value == 'desktop'){
+                if (!count.value){
+                    bg.classList.add('hidden')
+                    form.classList.add('hidden')
+                    hide.value
+                }else{
+                    hide.value += `;${count.value}`
+                }
+                
+            }
+            console.log(event.target,event.target.value)
+        })
+    }
+}
+bg.addEventListener('click', () =>{
+    bg.classList.add('hidden')
+    form.classList.add('hidden')
+})
